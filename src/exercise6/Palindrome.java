37f6e24c723a5f758fd9b5093058273be9e9ab86
MartoRam <mmmladenov1@gmail.com>
2020-11-05 00:01:39 +0200

    dada

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -1,9 +1,29 @@
 package exercise6;
 
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int number= input.nextInt();
+        int length=String.valueOf(number).length();
+        boolean p=false;
+        for (int i=0;i<length-1;i++)
+        {
+            if ((String.valueOf(number).charAt(i)+String.valueOf(number).charAt(length-1-i))/2==String.valueOf(number).charAt(i))
+            {
+                p=true;
+            }
+            else
+            {
+                p=false;
+                break;
+            }
+        }
+        System.out.println("Palindrome: "+p);
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -2,8 +2,28 @@ package exercise6;
 
 import java.util.Scanner;
 
-public class PasswordCheck {
-    public static void main(String[] args) {
+public class PasswordCheck
+{
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
+        var pass=input.next();
+        int digitcounter=0;
+        boolean valiable=false;
+        for (int i=0;i<pass.length();i++)
+        {
+            if (Character.isDigit(pass.charAt(i))==true)
+            {
+                digitcounter++;
+            }
+            if (String.valueOf(pass).length()>=8&&Character.isLetterOrDigit(pass.charAt(i))&&digitcounter>=2)
+            {
+                valiable=true;
+            }
+        }
+        if (valiable=true)
+        {
+            System.out.println("Password valid!");
+        }
     }
 }
