3e21347ef969c988ee5c3019ab2b1f11b50af777
MartoRam <mmmladenov1@gmail.com>
2020-11-05 00:18:15 +0200

    dada

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -10,18 +10,26 @@ public class PasswordCheck
         var pass=input.next();
         int digitcounter=0;
         boolean valiable=false;
+        System.out.println(pass.length());
         for (int i=0;i<pass.length();i++)
         {
-            if (Character.isDigit(pass.charAt(i))==true)
+            if (Character.isDigit(pass.charAt(i)))
             {
                 digitcounter++;
             }
-            if (String.valueOf(pass).length()>=8&&Character.isLetterOrDigit(pass.charAt(i))&&digitcounter>=2)
+        }
+        for (int i=0;i<pass.length();i++)
+        {
+            if (pass.length()>=8 &&Character.isLetterOrDigit(pass.charAt(i))&&digitcounter>=2)
             {
                 valiable=true;
             }
+            else
+                {
+                break;
+                }
         }
-        if (valiable=true)
+        if (valiable)
         {
             System.out.println("Password valid!");
         }
